378381b3a274aa9785c7d09b91b2e2aa72199424
Theodor Diaconu <diaconu.theodor@gmail.com>
2016-11-10 13:18:42 +0200

    refactored meteor extension

--- redis-oplog.js
+++ redis-oplog.js
@@ -1,10 +1,12 @@
 import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
-
 checkNpmVersions({
     'redis': '2.6.x',
     'sift': '3.2.x',
     'dot-object': '1.5.x',
 }, 'cultofcoders:redis-oplog');
+
+import publishWithRedis from './lib/publishWithRedis';
+import { RedisPipe, Events } from './lib/constants';
 
 import init from './lib/init';
 import SyntheticMutation from './lib/mongo/syntheticMutation';
@@ -13,4 +15,10 @@ const RedisOplog = {
     init
 };
 
-export { RedisOplog, SyntheticMutation }
+export {
+    RedisOplog,
+    SyntheticMutation,
+    publishWithRedis,
+    RedisPipe,
+    Events
+}
