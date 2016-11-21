81774341cf3d11f1ddf977b2fb1a42a614f7f8b4
Theodor Diaconu <diaconu.theodor@gmail.com>
2016-11-21 16:52:39 +0200

    bump to 1.0.8

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'cultofcoders:redis-oplog',
-    version: '1.0.7',
+    version: '1.0.8',
     // Brief, one-line summary of the package.
     summary: 'Replacement for Meteor\'s MongoDB oplog implementation',
     // URL to the Git repository containing the source code for this package.
