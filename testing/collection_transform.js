2e19d2d206374eae19593b46a97b5547f3c3f789
Theodor Diaconu <theodor.diaconu@cultofcoders.com>
2017-03-15 14:18:10 +0200

    Written test for #127

---
+++ testing/collection_transform.js
@@ -0,0 +1,14 @@
+function Foo () {}
+const fooCollection = new Mongo.Collection('foo', {
+    transform: function(document) {
+        return new Foo(document)
+    }
+});
+
+describe('Collection Transform', function () {
+    it('Should work with transform functions', function () {
+        fooCollection.insert({});
+        const foo = fooCollection.findOne();
+        assert.isTrue(foo instanceof Foo);
+    })
+});
--- testing/main.server.js
+++ testing/main.server.js
@@ -7,7 +7,8 @@ import '../lib/redis/testing';
 import './initial_add';
 import './collection_hooks.server';
 import './observe_callbacks.server';
-import './mutation_callbacks'
+import './mutation_callbacks';
+import './collection_transform';
 import { _ } from 'meteor/underscore';
 
 _.each(Collections, (Collection) => {
