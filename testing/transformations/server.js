a771d494d18db88e3139ab3dab12fc32a9548d2b
Theodor Diaconu <theodor.diaconu@cultofcoders.com>
2017-11-01 12:44:11 +0200

    added server-side test for transformations and observeChanges

--- testing/transformations/server.js
+++ testing/transformations/server.js
@@ -4,7 +4,9 @@ Meteor.publish('transformations_items', function () {
     return Items.find();
 });
 Meteor.publish('transformations_items_custom', function () {
-    return Items.find({}, {
+    return Items.find({
+        context: 'client',
+    }, {
         transform(doc) {
             doc.customServerTransform = true;
             return doc;
@@ -15,6 +17,24 @@ Meteor.publish('transformations_items_custom', function () {
 Meteor.methods({
     'transformations_boot'() {
         Items.remove({});
-        Items.insert({title: 'hello1'});
+        Items.insert({context: 'client', title: 'hello1'});
     }
+});
+
+
+describe('Transformations - Server Test', function () {
+    it('Should transform properly', function (done) {
+        const context = Random.id();
+        const handle = Items.find({
+            context,
+        }).observeChanges({
+            added(docId, doc) {
+                assert.isTrue(doc.defaultServerTransform);
+                handle.stop();
+                done();
+            }
+        });
+
+        Items.insert({context, title: 'hello2'});
+    })
 });
