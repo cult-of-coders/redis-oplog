63f1c558f210d0917a002b93394ea198c998b6e3
Theodor Diaconu <diaconu.theodor@gmail.com>
2018-02-14 08:30:09 +0200

    updates

--- package.js
+++ package.js
@@ -50,7 +50,7 @@ Package.onTest(function(api) {
     api.use('matb33:collection-hooks@0.8.4');
     api.use('alanning:roles@1.2.16');
 
-    api.use(['practicalmeteor:chai']);
+    api.use(['cultofcoders:mocha', 'practicalmeteor:chai']);
 
     api.mainModule('testing/main.server.js', 'server');
     api.addFiles('testing/publishComposite/boot.js', 'server');
