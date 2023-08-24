a0ffa1ea15099ccfca1d6465be34281cb0b28219
Jan Dvorak <storytellercz@proton.me>
2023-08-24 11:00:51 +0200

    Sync versionsFrom and tests

--- package.js
+++ package.js
@@ -17,7 +17,7 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom(['1.12.2', '2.8.1', '2.12']);
+    api.versionsFrom(['1.12.2', '2.8.1', '2.13']);
     api.use([
         'underscore',
         'ecmascript',
